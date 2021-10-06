7acefe017b7fb7112629921173af700c468aad6b
Nikita <nikita_dubyk@mail.ru>
2021-10-06 16:33:34 +0300

    add spinner, errorMessage

---
+++ src/components/errorMessage/ErrorMessage.js
@@ -0,0 +1,19 @@
+const ErrorMessage = () => {
+    return (
+        <>
+            <img
+                src='./error.gif'
+                alt='error'
+                style={{
+                    display: 'block',
+                    margin: '0 auto',
+                    width: '250px',
+                    height: '250px',
+                    objectFit: 'contain',
+                }}
+            />
+        </>
+    );
+};
+
+export default ErrorMessage;
