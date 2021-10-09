4644b08e805287796c07921ee206eb91b6685b0e
Nikita <nikita_dubyk@mail.ru>
2021-10-09 13:08:02 +0300

    add ErrorBoundary at the project

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -25,6 +25,11 @@ class CharInfo extends Component {
         }
     }
 
+    componentDidCatch(err, info) {
+        console.log(err, info);
+        this.setState({ error: true });
+    }
+
     updateChar = () => {
         const { charId } = this.props;
         if (!charId) {
