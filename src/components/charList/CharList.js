ca5f0e5f9802e584e0f43905626f395293e52674
Nikita <nikita_dubyk@mail.ru>
2021-11-04 16:58:32 +0300

    fixed bug on focuse char

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef, useMemo } from 'react';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import useMarvelServices from '../../services/MarvelServices';
 import Spinner from '../spinner/Spinner';
@@ -11,16 +11,12 @@ const setContent = (process, Component, newItemLoading) => {
     switch (process) {
         case 'waiting':
             return <Spinner />;
-            break;
         case 'loading':
             return newItemLoading ? <Component /> : <Spinner />;
-            break;
         case 'confirmed':
             return <Component />;
-            break;
         case 'error':
             return <ErrorMessage />;
-            break;
         default:
             throw new Error('Unexpected process state');
     }
@@ -36,6 +32,7 @@ const CharList = props => {
 
     useEffect(() => {
         onRequestChar(offset, true);
+        // eslint-disable-next-line
     }, []);
 
     const onRequestChar = (offset, initial) => {
@@ -110,9 +107,14 @@ const CharList = props => {
         );
     }
 
+    const elements = useMemo(() => {
+        return setContent(process, () => renderItem(charList), newItemLoading);
+        // eslint-disable-next-line
+    }, [process]);
+
     return (
         <div className='char__list'>
-            {setContent(process, () => renderItem(charList), newItemLoading)}
+            {elements}
             <button
                 className='button button__main button__long'
                 onClick={() => onRequestChar(offset)}
